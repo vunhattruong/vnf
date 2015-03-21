5db1129361b44212d511c2b5ab84fe4ab5a24c32
Truong Vu Nhat <vunhtruong@gmail.com>
2015-03-22 00:58:37 +0700

    This is the server NodeJS Version 0.1

---
+++ VNFServer/collectionDriver.js
@@ -0,0 +1,161 @@
+﻿// Lóp này tuong ứng các database helper,database driver có nhvụ tuong tác vói database 
+var ObjectID = require('mongodb').ObjectID; 
+
+// Constructor
+CollectionDriver = function(db){
+	this.db = db;
+}
+
+CollectionDriver.prototype.getCollection = function(collectionName,callback){
+	this.db.collection(collectionName,function(error,the_collection){
+		if(error)
+			callback(error);
+		else
+			callback(null,the_collection);
+	});
+}
+
+// get all docs in db
+CollectionDriver.prototype.findAll = function(collectionName,callback){
+	this.getCollection(collectionName,function(error,the_collection){
+		if(error)
+			callback(error);
+		else{
+			the_collection.find().toArray(function(error,results){
+				if(error)
+					callback(error);
+				else 
+					callback(null,results);
+			});
+		}
+	});
+}
+
+// Get document from db by id
+// * Since ObjectID is a BSON type and not a JSON type, 
+// you all have to convert any incoming strings to ObjectIDs if they are to be used when comparing against an �_id� field.
+CollectionDriver.prototype.get = function(collectionName,id,callback){
+	this.getCollection(collectionName,function(error,the_collection){
+		if(error)
+			callback(error);
+		else{
+			the_collection.findOne({'_id':ObjectID(id)},function(error,doc){
+				if(error)
+					callback(error);
+				else 
+					callback(null,doc);
+			});
+		}
+	});
+}
+
+// save new object
+CollectionDriver.prototype.save = function(collectionName,obj,callback){
+	this.getCollection(collectionName,function(error,the_collection){
+		if(error){
+			callback(error);
+		}else{
+			obj.created_at = new Date(); // add a field to the records the date it was created
+			the_collection.insert(obj,function(){
+				callback(null,obj);
+			});
+		}
+	});
+}
+
+CollectionDriver.prototype.update = function(collectionName,obj,entityId,callback){
+	this.getCollection(collectionName,function(error,the_collection){
+		if(error)
+			callback(error);
+		else{
+			obj._id = ObjectID(entityId); // convert to real object id
+			obj.updated_at = new Date(); // add fields update_at : data to object that have just been updated
+			the_collection.save(obj,function(error,doc){ // replace the old object with the new one
+				if(error)
+					callback(error);
+				else
+					callback(null,obj);
+			});
+		}
+	});
+}
+
+CollectionDriver.prototype.delete = function(collectionName,entityId,callback){
+	this.getCollection(collectionName,function(error,the_collection){
+		if(error)
+			callback(error);
+		else{
+			the_collection.remove({'_id': ObjectID(entityId)},function(error,doc){
+				if(error)
+					callback(error);
+				else
+					callback(null,doc);
+			});
+		}
+	});
+}
+
+//Calculate distance between 2 coordinates
+function calDistance(lat1, lon1, lat2, lon2) {
+	/** Converts numeric degrees to radians */
+	if (typeof (Number.prototype.toRad) === "undefined") {
+		Number.prototype.toRad = function() {
+			return this * Math.PI / 180;
+		}
+	}
+
+	var R = 6371; // km
+	var dLat = (lat2 - lat1).toRad();
+	var dLon = (lon2 - lon1).toRad();
+	var lat1 = lat1.toRad();
+	var lat2 = lat2.toRad();
+
+	var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2)
+			* Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	var d = R * c; // Unit: km
+	return d * 1000; // Unit: km*1000 = m
+}
+
+//Perform a collection query
+CollectionDriver.prototype.query = function(collectionName, query, callback) { //1
+    this.getCollection(collectionName, function(error, the_collection) { //2
+      if( error ) callback(error)
+      else {
+    	  // Nếu là request lấy các dia diem gan do
+    	  if( (query.lat != null) && (query.lon != null) && (query.radius != null) ){
+    		  the_collection.find().toArray(function(error,results){
+    			 if(error) callback(error)
+    			 else{
+    				 //callback(null,results)
+    				 console.log(query.radius);
+    				 var resultJSON = "[";
+    				     				 
+    				 for (var i = 0; i < results.length; i++) {
+    					var result = calDistance(parseFloat(query.lat), parseFloat(query.lon),
+    							parseFloat(results[i].lat), parseFloat(results[i].lon));
+    					console.log("***"+results[i].name + " : "+result);
+    					if( result <= parseInt(query.radius) ){
+    						console.log(results[i].name + " : " + result);
+    						resultJSON += JSON.stringify(results[i])+",";
+    					}
+					}
+    				console.log(resultJSON.substring(0,resultJSON.length-1)+"]");
+    				callback(null,JSON.parse(resultJSON.substring(0,resultJSON.length-1)+"]"));
+    			 }
+    		  });
+    	  } else{ // Nếu là request lấy tất cả (k có query)
+    		  console.log("Dzo");
+    		  console.log(query);
+        	the_collection.find(query).toArray(function(error, results) { //3
+        		if( error ) callback(error)
+        		else callback(null, results)
+        	});
+    	  }
+    	
+      }
+    });
+};
+
+// This line declares the exposed, or exported, entities to other applications that list collectionDriver.js as a required module.
+exports.CollectionDriver = CollectionDriver;
---
+++ VNFServer/index.js
@@ -0,0 +1,157 @@
+var http = require('http'),
+express = require('express'),
+path = require('path'),
+MongoClient = require('mongodb').MongoClient,
+Server = require('mongodb').Server,
+CollectionDriver = require('./collectionDriver').CollectionDriver;
+
+// Create express app
+var app = express();
+
+// set default port to 3000
+app.set('port',process.env.PORT || 3000);
+
+// set the route to views
+app.set('views',path.join(__dirname,'views'));
+// set view engine = jade
+app.set('view engine','jade');
+
+// tell express to parse the incoming data, if it's json -> create json object
+// This way req.body can be passed directly to the driver code as a JavaScript
+// object.
+app.use(express.bodyParser());
+
+var mongoHost = 'localHost';
+var mongoPort = 27017;
+var collectionDriver;
+
+// create mongoclient and etablish a connection
+var mongoClient = new MongoClient(new Server(mongoHost,mongoPort));
+mongoClient.open(function(err,mongoClient){
+	if(!mongoClient){
+		console.error('Error! Exiting... Must start MongoDB first');
+		process.exit(1);
+	}
+	var db = mongoClient.db('MyDatabase');
+	collectionDriver = new CollectionDriver(db);
+});
+
+// tells Express to use the middleware express.static which serves up static
+// files in response to incoming requests.
+// path.join(__dirname, 'public') maps the local subfolder public to the base
+// route /;
+// it uses the Node.js path module to create a platform-independent subfolder
+// string.
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/', function (req, res) {
+  res.send('<html><body><h1>Node Server</h1></body></html>');
+});
+	
+app.get('/:collection/:entity',function(req,res){
+	var params = req.params;
+	var entity = params.entity;
+	var collection = params.collection;
+	if(entity){
+		collectionDriver.get(collection,entity,function(error,objs){
+			if(error)
+				res.send(400,error);
+			else
+				res.send(200,objs);
+		});
+	}else{
+		res.send(400,{error:'bad url',url:req.url});
+	}
+});
+
+app.get('/:collection', function(req, res, next) {  
+	var params = req.params;
+	var query = req.query; // 1 (query is in JSON type)
+	if (query) {
+		collectionDriver.query(req.params.collection, query, returnCollectionResults(req,res)); // 3
+	} else {
+	    collectionDriver.findAll(req.params.collection, returnCollectionResults(req,res)); // 4
+	}
+});
+	 
+function returnCollectionResults(req, res) {
+    return function(error, objs) { // 5
+        if (error) { res.send(400, error); }
+	        else { 
+                    if (req.accepts('html')) { // 6
+                        res.render('data',{objects: objs, collection:
+						req.params.collection});
+                    } else {
+                        res.set('Content-Type','application/json');
+                        res.send(200, objs);
+                }
+        }
+    };
+};
+
+// insert
+app.post('/:collection',function(req,res){
+	var object = req.body; // phần body đã dc parse thành json object rồi
+	var collection = req.params.collection;
+	collectionDriver.save(collection,object,function(err,docs){
+		if(err){			
+			res.send(400,err);
+		}else 
+			res.send(201,docs); // create resource successfully
+	});
+});
+
+// update
+app.put('/:collection/:entity',function(req,res){
+	var params = req.params;
+	var entity = params.entity;
+	var collection = params.collection;
+	if(entity){
+		collectionDriver.update(collection,req.body,entity,function(error,objs){
+			if(error)
+				res.send(400,error);
+			else
+				res.send(200,objs);
+		});
+	}else{
+		res.send(400,error);
+	}
+});
+
+// delete
+app.delete('/:collection/:entity',function(req,res){
+	var params = req.params;
+	var entity = params.entity;
+	var collection = params.collection;
+	if(entity){
+		collectionDriver.delete(collection,entity,function(error,objs){
+			if(error)
+				res.send(400,error);
+			else
+				res.send(200,objs);
+		});
+	}else{
+		var error = {'message':'Can not DELETE the whole collection'};
+		res.send(400,error);
+	}
+});
+
+app.use(function (req,res) {
+    res.render('404', {url:req.url});
+});
+
+http.createServer(app).listen(app.get('port'),function(){
+	console.log('Express server is running on port '+app.get('port'));
+});
+
+// Insert
+// curl -H "Content-Type: application/json" -X POST -d "{\"title\":\"Hello
+// World\"}" http://localhost:3000/items
+
+// Update (thay id vào {_id})
+// curl -H "Content-Type: application/json" -X PUT -d "{\"title\":\"Good Golly
+// Miss Molly\"}" http://localhost:3000/items/{_id}
+
+// Delete (thay id vào {_id})
+// curl -H "Content-Type: application/json" -X DELETE
+// http://localhost:3000/items/{_id}
