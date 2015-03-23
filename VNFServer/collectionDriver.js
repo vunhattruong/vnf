213036a89beb83c69d399bcab82095a982153e3b
Truong Vu Nhat <vunhtruong@gmail.com>
2015-03-23 21:41:31 +0700

    + Tim kiem dia diem bang mon an
    http://127.0.0.1:3000/locs?lat=10.952915&lon=106.820954&radius=5000&dsh=cha+lui
    
    + Lay dia diem theo id 
    
    http://127.0.0.1:3000/550e4f4729bc80b49044c63f
    
    + Tim kiem dia diem theo ban kinh
    
    http://127.0.0.1:3000/locs?lat=10.952915&lon=106.820954&radius=5000
    
    + Lay tat ca cac dia diem trong csdl
    
    http://127.0.0.1:3000/locs

--- VNFServer/collectionDriver.js
+++ VNFServer/collectionDriver.js
@@ -33,7 +33,7 @@ CollectionDriver.prototype.findAll = function(collectionName,callback){
 
 // Get document from db by id
 // * Since ObjectID is a BSON type and not a JSON type, 
-// you all have to convert any incoming strings to ObjectIDs if they are to be used when comparing against an �_id� field.
+// you�ll have to convert any incoming strings to ObjectIDs if they�re to be used when comparing against an �_id� field.
 CollectionDriver.prototype.get = function(collectionName,id,callback){
 	this.getCollection(collectionName,function(error,the_collection){
 		if(error)
@@ -117,41 +117,190 @@ function calDistance(lat1, lon1, lat2, lon2) {
 	return d * 1000; // Unit: km*1000 = m
 }
 
+// Bỏ dấu tiếng việt nhằm phục vụ cho viec tim kiem vd: chả lụi -> cha lui
+function change_alias( alias )
+{
+    var str = alias;
+    str= str.toLowerCase(); 
+    str= str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ  |ặ|ẳ|ẵ/g,"a"); 
+    str= str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g,"e"); 
+    str= str.replace(/ì|í|ị|ỉ|ĩ/g,"i"); 
+    str= str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ  |ợ|ở|ỡ/g,"o"); 
+    str= str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g,"u"); 
+    str= str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g,"y"); 
+    str= str.replace(/đ/g,"d"); 
+    str= str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'| |\"|\&|\#|\[|\]|~|$|_/g,"-");
+    /* tìm và thay thế các kí tự đặc biệt trong chuỗi sang kí tự - */
+    str= str.replace(/-+-/g,"-"); //thay thế 2- thành 1-
+    str= str.replace(/^\-+|\-+$/g,""); 
+    //cắt bỏ ký tự - ở đầu và cuối chuỗi 
+    return str;
+}
+
 //Perform a collection query
 CollectionDriver.prototype.query = function(collectionName, query, callback) { //1
+	
     this.getCollection(collectionName, function(error, the_collection) { //2
       if( error ) callback(error)
       else {
+    	  
+    	  
     	  // Nếu là request lấy các dia diem gan do
     	  if( (query.lat != null) && (query.lon != null) && (query.radius != null) ){
+    		  
+    		  
     		  the_collection.find().toArray(function(error,results){
-    			 if(error) callback(error)
-    			 else{
-    				 //callback(null,results)
-    				 console.log(query.radius);
-    				 var resultJSON = "[";
-    				     				 
-    				 for (var i = 0; i < results.length; i++) {
-    					var result = calDistance(parseFloat(query.lat), parseFloat(query.lon),
-    							parseFloat(results[i].lat), parseFloat(results[i].lon));
-    					console.log("***"+results[i].name + " : "+result);
-    					if( result <= parseInt(query.radius) ){
-    						console.log(results[i].name + " : " + result);
-    						resultJSON += JSON.stringify(results[i])+",";
-    					}
-					}
-    				console.log(resultJSON.substring(0,resultJSON.length-1)+"]");
-    				callback(null,JSON.parse(resultJSON.substring(0,resultJSON.length-1)+"]"));
-    			 }
+     			 if(error) callback(error)
+     			 else{
+     				 //callback(null,results)
+     				 
+     				 var countdsh = 0; // biến đếm món ăn
+     				 var count = 0; // biến đếm địa điểm    				 
+     				 
+     				 var resultJSON = "["; // chuỗi json trả về địa điểm
+     				 var resultJSONdsh = "["; // chuỗi json trả về địa điểm khi có món ăn
+     				 
+     				 for (var i = 0; i < results.length; i++) {
+     					var result = calDistance(parseFloat(query.lat), parseFloat(query.lon),
+     							parseFloat(results[i].location.lat), parseFloat(results[i].location.lon));
+     					if( result <= parseInt(query.radius) ){
+     						count++;
+     						console.log(results[i].name + " : " + result);
+     						resultJSON += JSON.stringify(results[i])+",";
+     						
+     						if(query.dsh != null){ // ktra xem có querystring dsh hay k?
+     							for (var j = 0; j < results[i].dishes.length; j++) {
+     								if(change_alias(results[i].dishes[j].name.toLowerCase()) == change_alias(query.dsh.toLowerCase())){ // ham so sanh chuoi bi sai
+     									countdsh++;
+     									resultJSONdsh += JSON.stringify(results[i])+",";
+     								}
+     							}
+     						}
+     						
+     					}
+ 					}
+     				
+     				 if(query.dsh != null ){ // truog hop co querystring dsh (món ăn)
+     					 if(countdsh != 0){
+     						 console.log(resultJSON.substring(0,resultJSON.length-1)+"]");
+     						 callback(null,JSON.parse(resultJSON.substring(0,resultJSON.length-1)+"]"));
+        				  	}else {
+        				  		 callback(null,"");
+        				  	}
+     				 }else { // truog hop k co querystring dsh
+     					 if(count != 0){
+     						 console.log(resultJSON.substring(0,resultJSON.length-1)+"]");
+     						 callback(null,JSON.parse(resultJSON.substring(0,resultJSON.length-1)+"]"));
+     					 }else {
+     						 callback(null,"");
+     					 }
+     				
+     				 }
+     				
+     			 }
+     		  });
+    		  
+    		  
+    	  }else if(query.dsh != null){ // query string : dsh -> get locs by dsh name
+    		  
+    		  the_collection.find().toArray(function(error, results) { //3
+    			  if( error ) callback(error);
+    			  else{ 
+    				  var resultJSON = "[";
+    				  var count = 0;
+    				  
+    				  // GET LOCS BY FOOD
+    				  for (var i = 0; i < results.length; i++) {
+    					  for (var j = 0; j < results[i].dishes.length; j++) {
+    						  if(results[i].dishes[j].name.toLowerCase() == query.dsh.toLowerCase()){ // ham so sanh chuoi bi sai
+    							  count++;
+    							  resultJSON += JSON.stringify(results[i])+",";
+    						  }
+    					  }
+    				  }
+    				  
+    				  if(count != 0){
+    					  console.log(resultJSON.substring(0,resultJSON.length-1)+"]");
+    					  callback(null,JSON.parse(resultJSON.substring(0,resultJSON.length-1)+"]"));
+    				  }else {
+    					  callback(null,"");
+    				  }
+			  
+    			  }
     		  });
+    		  
+			  
+    	  }else if(query.test != null){ // Test database
+    		  
+    		  the_collection.find().toArray(function(error, results) { //3
+    			  if( error ) callback(error)
+    			  else{ 
+    				  callback(null,results);
+    				  
+    				  // GET ARRAY ELEMENTS
+    				  /*console.log(JSON.stringify(results[0].name));
+    				  console.log(JSON.stringify(results[0].location.lat));
+    				  console.log(JSON.stringify(results[0].location.lon));
+    				  console.log(JSON.stringify(results[0].comments[0].username));
+    				  console.log(JSON.stringify(results[0].comments[0].detail));*/
+    				  
+    				  /*results[0].comments.push("{test:test}");
+    				  console.log(JSON.stringify(results[0].comments));
+    				  */
+    				  
+    				  // INSERT ARRAY ELEMENTS
+    				  /*var obj = results[0];
+    				  obj._id = results[0]._id; // convert to real object id
+    				  var text = '{ "employees" : "test" }';
+    				  obj.comments.push(JSON.parse(text));
+    				  the_collection.save(obj,function(error,doc){ // replace the old object with the new one
+    						if(error)
+    							callback(error);
+    						else
+    							callback(null,obj);
+    				  });*/
+    				  
+    				  // UPDATE ARRAY ELEMENTS
+    				  /*var obj = results[0];
+    				  obj._id = results[0]._id; // convert to real object id
+    				  var text = '{ "employees" : "test" }';
+    				  obj.comments[0] = JSON.parse(text);
+    				  the_collection.save(obj,function(error,doc){ // replace the old object with the new one
+    						if(error)
+    							callback(error);
+    						else
+    							callback(null,obj);
+    				  });*/
+    				  
+    				  // DELETE ARRAY ELEMENTS
+    				 /* var obj = results[0];
+    				  obj._id = results[0]._id; // convert to real object id
+    				  var text = '{ "employees" : "test" }';
+    				  obj.comments.splice(0,1);
+    				  the_collection.save(obj,function(error,doc){ // replace the old object with the new one
+    						if(error)
+    							callback(error);
+    						else
+    							callback(null,obj);
+    				  });*/
+    					
+    			  }
+        	  });
+    		  
+    		  /*collection.find(query).toArray(function(error, results) { //3
+          		if( error ) callback(error)
+          		else callback(null, results)
+          	  });*/
+    		  
+    		  
     	  } else{ // Nếu là request lấy tất cả (k có query)
-    		  console.log("Dzo");
     		  console.log(query);
-        	the_collection.find(query).toArray(function(error, results) { //3
+    		  the_collection.find(query).toArray(function(error, results) { //3
         		if( error ) callback(error)
         		else callback(null, results)
-        	});
+        	  });
     	  }
+    	  
     	
       }
     });
