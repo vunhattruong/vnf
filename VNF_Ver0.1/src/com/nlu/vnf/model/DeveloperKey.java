762269a9791e4b7e6f6b1bb606a775df843f177a
Truong Vu Nhat <vunhtruong@gmail.com>
2015-05-04 22:56:38 +0700

    Update Android app

--- VNF_Ver0.1/src/com/nlu/vnf/model/DeveloperKey.java
+++
@@ -1,7 +0,0 @@
-package com.nlu.vnf.model;
-
-public class DeveloperKey {
-
-	public static String URL_HOME = "http://192.168.56.1:3000/locs?lat=10.952915&lon=106.820954&radius=5000&dsh=bit+tet";
-
-}
